08844faad6acdbe1fabaf606311b4ae846f14e1b
Tyrique Daniel <37724661+CodingOni@users.noreply.github.com>
2020-09-23 13:27:48 -0600

    Fixed Header img Name

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bc0e9ec80d5e3727de5a";
+/******/ 	var hotCurrentHash = "70fada865304c9dbe772";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
